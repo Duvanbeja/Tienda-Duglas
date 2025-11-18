c1c0a2477adc77d841f0ff6f25371d66e349564b
Sasct <fdbejarano@ucompensar.edu.co>
2025-11-17 21:58:31 -0500

    lista

--- Tienda.java
+++ Tienda.java
@@ -1,4 +1,8 @@
 public class Tienda{
+    static String[] nombres = new String[50];
+    static int[] cantidad = new int[50];
+    static int[] precio = new int[50];
+    static int totalProductos = 0;
     public static void main(String[] args) {
         
     }
