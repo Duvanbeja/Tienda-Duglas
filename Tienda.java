a5c9e0cd9f2b7bc00b5cef9e070f9cbff6afc3bf
jorge <jeliecerpinzon.edu.co>
2025-11-17 23:06:00 -0500

    Laura

--- Tienda.java
+++ Tienda.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Tienda{
     static String[] nombres = new String[50];
     static int[] cantidad = new int[50];
@@ -8,8 +10,28 @@ public class Tienda{
     static int[] Cantidad_Ventas = new int[100];
     static int[] Total_Ventas = new int[100];
     static int Totales_Vendidos = 0;
-    public static void main(String[] args) {
-        
+    Scanner Ahuyama = new Scanner(System.in);
     }
+
+    public static void main(String[] args) {
+    
+        int option = 0;
+        
+        while(option != 5){ 
+        System.out.println ("Bienvenido a la tienda Douglas");
+        System.out.println("¿Que deseas hacer?");
+        System.out.println ("1 Ingresar nuevo producto");
+        System.out.println ("2 Realizar venta");
+        System.out.println ("3 Generar reporte de venta");
+        System.out.println ("4 Generar reporte de stock");
+        System.out.println ("5 Salir");
+        System.out.print ("Elija que deseas hacer");
+        
+
+
+
+    }
+
+
 
 }
