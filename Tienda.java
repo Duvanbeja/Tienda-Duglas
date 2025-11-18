eb74e137aa6ccf3c6785493a8fac3edc0acd0195
Laura <lvquiceno@ucompensar.edu.co>
2025-11-17 22:16:35 -0500

    segunda tabla

--- Tienda.java
+++ Tienda.java
@@ -3,7 +3,13 @@ public class Tienda{
     static int[] cantidad = new int[50];
     static int[] precio = new int[50];
     static int totalProductos = 0;
+
+    static String[] Nombre_ventas = new String[100];
+    static int[] Cantidad_Ventas = new int[100];
+    static int[] Total_Ventas = new int[100];
+    static int Totales_Vendidos = 0;
     public static void main(String[] args) {
         
     }
+
 }
